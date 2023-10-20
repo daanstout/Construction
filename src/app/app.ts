78a714971ba5bb2b17e2af81fcf9cb3b9149176d
Daan Stout <daanstoutdev@gmail.com>
2023-10-20 13:07:49 +0200

    Fixed the skill throwing an error

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,26 @@
 import { BuildingLibrary, IBuildingLibrary } from "./construction/building/building-library";
 import { config } from "./construction/config/config";
 import { ConfigReader } from "./construction/config/config-reader";
-import { Construction } from "./construction/construction";
+import { Construction, ConstructionSkillData } from "./construction/construction";
+import { languages } from "./language/index";
 
 declare global {
     interface Game {
         construction: Construction;
+    }
+
+    interface SkillIDDataMap {
+        'nifeConstruction:Construction': ConstructionSkillData;
+    }
+
+    interface SkillObject<T> {
+        Construction: T;
+    }
+
+    namespace Modding {
+        interface ModContext {
+            onInterfaceAvailable: (callback: Modding.LifecycleCallback) => void;
+        }
     }
 }
 
@@ -15,17 +30,21 @@ export class App {
     constructor(private readonly modContext: Modding.ModContext, private readonly game: Game) { }
 
     public async init() {
-        this.modContext.loadTemplates('Construction/Construction.html');
+        // this.modContext.loadTemplates('construction/construction.html');
+        // // for (const [key, value] of Object.entries<string>(languages['en'])) {
+        // //     loadedLangJson['SKILL_NAME'][key] = value;
+        // // }
 
-        const configReader = new ConfigReader(config);
-        this.buildingLibrary = new BuildingLibrary(configReader);
+        // this.game.construction = this.game.registerSkill(this.game.registeredNamespaces.getNamespace('nifeConstruction')!, Construction);
 
-        this.game.construction = this.game.registerSkill(this.game.registeredNamespaces.getNamespace('nifeConstruction')!, Construction);
+        // await this.modContext.gameData.addPackage('data.json');
 
-        this.modContext.gameData.addPackage('data.json');
+        // // const configReader = new ConfigReader(config);
+        // // this.buildingLibrary = new BuildingLibrary(configReader);
 
-        this.modContext.onInterfaceReady(() => {
-            document.getElementById('main-container')?.append(getTemplateNode('nife-construction'));
-        })
+        // this.modContext.onInterfaceAvailable(() => {
+        //     document.getElementById('main-container')?.append(getTemplateNode('nife-construction'));
+        //     // ui.createStatic('nife-construction', document.getElementById('main-container')!);
+        // })
     }
 }
